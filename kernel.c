4ab60c014c215f40ab69c80cdc72c9249345d15f
amzy-0 <azymiamin@gmail.com>
2020-11-05 03:44:43 +0330

    type speed fixed

--- kernel.c
+++ kernel.c
@@ -3,7 +3,7 @@
 #include "includes/char.h"
 uint32 vga_index;
 static uint32 next_line_index = 1;
-uint8 g_fore_color = WHITE, g_back_color = BLUE;
+uint8 g_fore_color = WHITE, g_back_color = BLACK;
 int digit_ascii_codes[10] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
 
 /*
@@ -22,8 +22,7 @@ vga_print_char:
   ret
 
 */
-uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color)
-{
+uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color){
   uint16 ax = 0;
   uint8 ah = 0, al = 0;
 
@@ -38,8 +37,7 @@ uint16 vga_entry(unsigned char ch, uint8 fore_color, uint8 back_color)
   return ax;
 }
 
-void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color)
-{
+void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color){
   uint32 i;
   for(i = 0; i < BUFSIZE; i++){
     (*buffer)[i] = vga_entry(NULL, fore_color, back_color);
@@ -51,8 +49,7 @@ void clear_vga_buffer(uint16 **buffer, uint8 fore_color, uint8 back_color)
 
 // set terminal color
 
-void init_vga(uint8 fore_color, uint8 back_color)
-{
+void init_vga(uint8 fore_color, uint8 back_color){
   vga_buffer = (uint16*)VGA_ADDRESS;
   clear_vga_buffer(&vga_buffer, fore_color, back_color);
   g_fore_color = fore_color;
@@ -62,8 +59,7 @@ void init_vga(uint8 fore_color, uint8 back_color)
 
 //this function print a new line on your termninal like '\n'
 
-void newline_on_terminal()
-{
+void newline_on_terminal(){
   if(next_line_index >= 55){
     next_line_index = 0;
     clear_vga_buffer(&vga_buffer, g_fore_color, g_back_color);
@@ -72,14 +68,12 @@ void newline_on_terminal()
   next_line_index++;
 }
 
-void print_char(char ch)
-{
+void print_char(char ch){
   vga_buffer[vga_index] = vga_entry(ch, g_fore_color, g_back_color);
   vga_index++;
 }
 
-void print_on_terminal(char *str)
-{
+void print_on_terminal(char *str){
   uint32 index =0;
   while(str[index]){
     print_char(str[index]);
@@ -87,15 +81,13 @@ void print_on_terminal(char *str)
   }
 }
 
-void print_int(int num)
-{
+void print_int(int num){
   char str_num[digit_count(num)+1];
   itoa(num, str_num);
   print_on_terminal(str_num);
 }
 
-uint8 inb(uint16 port)
-{
+uint8 inb(uint16 port){
   uint8 ret;
   asm volatile("inb %1, %0" : "=a"(ret) : "d"(port));
   return ret;
@@ -105,10 +97,9 @@ void outb(uint16 port, uint8 data)
 {
   asm volatile("outb %0, %1" : "=a"(data) : "d"(port));
 }
-// get keys from your keyboard 
-//and read 
-char get_input_prompt()
-{
+// get keys from your keyboard
+//and read
+char get_input_prompt(){
   char ch = 0;
   while((ch = inb(KEYBOARD_PORT)) != 0){
     if(ch > 0)
@@ -122,8 +113,7 @@ keep the cpu busy for doing nothing(nop)
 so that io port will not be processed by cpu
 here timer can also be used, but lets do this in looping counter
 */
-void wait_for_io(uint32 timer_count)
-{
+void wait_for_io(uint32 timer_count){
   while(1){
     asm volatile("nop" /*do noting*/  );
     timer_count--;
@@ -131,99 +121,113 @@ void wait_for_io(uint32 timer_count)
       break;
     }
 }
-//sleep function 
+//sleep function
 void sleep(uint32 timer_count)
 {
   wait_for_io(timer_count);
 }
 
-// get and read the keys  and kernel reaction 
+// get and read the keys  and kernel reaction
 void input()
 {
   char ch = 0;
   char keycode = 0;
   do{
+    int enter_cuonter=1;
     keycode = get_input_prompt();
     // if client press enter (KEY)
     if(keycode == KEY_ENTER){
-
-      //for (size_t i = 0; i < count; i++) {
-        
-        newline_on_terminal();
+      newline_on_terminal();
 
       print_on_terminal("[ root @ charleX ]# ");
 
+      enter_cuonter++;
+
     }
+    /*
+     * if client press backspace for delete a single
+     */
+    else if(keycode == KEY_BACKSPACE){
+        vga_index = vga_index-1;
+        print_on_terminal(" ");
+        vga_index = vga_index-1;
+        get_input_prompt();
+    }
+
     //if client press arrow up (KEY)
     else if(keycode == KEY_UP){
           newline_on_terminal();
-          print_on_terminal("KEY UP PRESSED : ");
-          newline_on_terminal();
+          print_on_terminal("KEY : UP ARROW | STATUS: PRESSED; ");
           print_on_terminal("this key not work in this terminal !");
           newline_on_terminal();
-
-          print_on_terminal("just type the command ! ");
           newline_on_terminal();
-
           print_on_terminal("[ root @ charleX ]# ");
+          enter_cuonter++;
           get_input_prompt();
     }
     //if client press arrow down (KEY)
     else if(keycode == KEY_DOWN){
           newline_on_terminal();
-          print_on_terminal("KEY DOWN PRESSED : ");
-          newline_on_terminal();
+          print_on_terminal("KEY : DOWN ARROW | STATUS: PRESSED; ");
           print_on_terminal("this key not work in this terminal !");
           newline_on_terminal();
-
-          print_on_terminal("just type the command ! ");
           newline_on_terminal();
-
           print_on_terminal("[ root @ charleX ]# ");
+          enter_cuonter++;
           get_input_prompt();
     }
     //if client press TAB (KEY)
     else if(keycode == KEY_TAB){
           newline_on_terminal();
-          print_on_terminal("TAB PRESSED : ");
+          print_on_terminal("KEY : TAB | STATUS : PRESSED; ");
           newline_on_terminal();
-          print_on_terminal("for shutdown : PWOEROFF");
+          print_on_terminal("[ESC (key)] : exit, [BACKSPACE (key)] : delete a charecter ");
           newline_on_terminal();
-          print_on_terminal("for sleep : SLEEPOS");
           newline_on_terminal();
-
           print_on_terminal("[ root @ charleX ]# ");
+          enter_cuonter++;
           get_input_prompt();
     }
     //if client press ESC (KEY)
     else if(keycode == KEY_ESC){
         init_vga(RED,BLACK);
-        newline_on_terminal();
-        newline_on_terminal();
-        newline_on_terminal();
-        print_on_terminal("EXIT !");
-        newline_on_terminal();
+        print_on_terminal("EXIT ! : ");
         print_on_terminal("if you wnat use CHARLEX-OS => please REBOOT your machine !");
-        get_input_prompt();
-        input();
+        break;
+        enter_cuonter++;
+        newline_on_terminal();
     }
+
     else{
       ch = get_ascii_char(keycode);
       print_char(ch);
+
+
     }
+    /*
+     * NO SCROLL TERMINAL (static prompt )
+     */
+    if(enter_cuonter<5){
+      enter_cuonter = enter_cuonter *2;
+      next_line_index =next_line_index - enter_cuonter;
+    }
+    else
+{
+      next_line_index =next_line_index - enter_cuonter;
+    }
+
+
     for (int i = 0; i < 1; i++){
-      sleep(0x09FFFFFF);
+      sleep(0x0CFFFFFF);/* keyboard type speed for real machine (((32bit))) */
     }
-    
+
   }while(ch > 0);
+
 }
 
-// this function load logo 
+// this function load logo
 
 void logo(){
-  //old logo !
-  //==============================================================
-
   // print_on_terminal("      _                _     __  __");
   // newline_on_terminal();
 
@@ -252,8 +256,7 @@ void logo(){
   // newline_on_terminal();
 
   // print_on_terminal("        	 \\___/|____/    ");
-  
-  //==============================================================
+
   print_on_terminal("                                                        ,/{}");
   newline_on_terminal();
 
@@ -289,7 +292,7 @@ void logo(){
 
   print_on_terminal(" \"88__/ 888  888  \"88_-888 888    888  \"88___/  /    Y88b`,     _-----___    _,'");
   newline_on_terminal();
-  
+
   print_on_terminal("                                                         / /--__  ~~--__  `~,~");
   newline_on_terminal();
 
@@ -298,15 +301,14 @@ void logo(){
 
   print_on_terminal("------------------------------------------------------' ");
   newline_on_terminal();
- 
 }
+
 // kernel entery point !!
-void kernel_entry()
-{
+void kernel_up(){
 
 
   // color of terminal(you can change it if you want :D )
-  init_vga(GREEN, BLACK);
+  init_vga(WHITE, BLACK);
   newline_on_terminal();
   logo();
 
@@ -315,22 +317,24 @@ void kernel_entry()
 
   newline_on_terminal();
 
+
   print_on_terminal("|------------------|");
   newline_on_terminal();
   print_on_terminal("!Welcome to charleX!");
   newline_on_terminal();
+  print_on_terminal("!    version1.0    !");
+  newline_on_terminal();
   print_on_terminal("|------------------|");
 
-  for (int i = 0; i < 20; i++){
-//    sleep(0x02FFFFFA); // sleep for logo  ;;
-      sleep(0x02FFFFFFA); // sleep for  ;;
+  for (int i = 0; i < 40; i++){
+    sleep(0x02FFFFFF); // sleep for logo to load ;;
   }
-  //color set to green and black 
-  init_vga(GREEN , BLACK);
+  //color set to green and black
+  init_vga(WHITE, BLACK);
   newline_on_terminal();
-  // input() for   infinity loop (terminal loop infinity) 
+  // input() for   infinity loop (terminal loop infinity)
   print_on_terminal("[ root @ charleX ]# ");
+
   input();
 
-     
 }
