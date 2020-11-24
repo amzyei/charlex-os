4c46bd0d2ad899bef75ad0c2bbf8cf442b5200fc
amzy-0 <azymiamin@gmail.com>
2020-11-24 15:54:37 +0330

    version fixed (version 1.0 -> version 0.1)

--- virtual.c
+++ virtual.c
@@ -246,7 +246,7 @@ void kernel_up(){
   newline_on_terminal();
   print_on_terminal("!Welcome to charleX!");
   newline_on_terminal();
-  print_on_terminal("!    version1.0    !");
+  print_on_terminal("!    version 0.1   !");
   newline_on_terminal();
   print_on_terminal("|------------------|");
 
