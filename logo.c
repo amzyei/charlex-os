87a78ac11f264fff3fc962be74982d526ca7e47d
amzyei <amzyei@localhost.localdomain>
2024-06-07 07:35:33 +0330

    version 0.2

--- logo.c
+++ logo.c
@@ -39,7 +39,7 @@ newline_on_terminal();
     newline_on_terminal();
     print_on_terminal("!Welcome to charleX!");
     newline_on_terminal();
-    print_on_terminal("!    version 0.1   !");
+    print_on_terminal("!    version 0.2   !");
     newline_on_terminal();
     print_on_terminal("|------------------|");
     newline_on_terminal();
